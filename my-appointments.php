9ed753c325c6738f51c2e51fa39e6c36b96dc0b9
Raghavendra <nraghavendra89@gmail.com>
2022-08-14 11:58:53 +0530

    Yay! First commit.

---
+++ my-appointments.php
@@ -0,0 +1,14 @@
+/**
+ * Plugin Name:       My Appointments Plugin
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Appointments booking plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Raghavendra N
+ * Author URI:        https://author.example.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       my-appointments-plugin
+ * Domain Path:       /languages
+ */
